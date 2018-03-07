b482cbf50dc9af34aff7b1322e8a098a44b309cb
jackparra253 <jack.parra253@gmail.com>
2018-03-07 14:41:14 -0500

    ok
    ok

---
+++ ejercicio020.go
@@ -0,0 +1,28 @@
+//Confeccionar un programa que lea por teclado tres números distintos y nos muestre el mayor.
+
+package main
+
+import "fmt"
+
+func main(){
+
+	var numeroUno, numeroDos, numeroTres int
+
+	fmt.Print("Ingrese un número:")
+	fmt.Scan(&numeroUno)
+
+	fmt.Print("Ingrese un número: ")
+	ftm.Scan(&numeroDos)
+
+	fmt.Print("Ingrese un número: ")
+	fmt.Scan(&numeroTres)
+
+	if numerouno > numeroDos && numeroUno > numeroTres {
+		fmt.Print("El número mayor es: " , numeroUno)
+	} else if numeroDos > numeroTres && numeroDos > numeroUno {
+		fmt.Print("El número mayor es: ", numeroDos)
+	} else if numeroTres > numeroUno && numeroTres > numeroDos {
+		fmt.Print("El número mayor es: ").
+	}
+
+}
